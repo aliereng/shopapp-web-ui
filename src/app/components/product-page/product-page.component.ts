38a0996cb05ce3258581b33ad3eea27a900f7f66
aliereng <alierengonulcelen@gmail.com>
2023-03-28 16:18:48 +0300

    product page color-size-price opt. complete

--- src/app/components/product-page/product-page.component.ts
+++ src/app/components/product-page/product-page.component.ts
@@ -13,14 +13,28 @@ export class ProductPageComponent implements OnInit {
   id: String = "";
   slug: String = "";
   product!: Product; 
+  productColor!: String;
+  productSize!: String;
+  productPrice!: Number;
   selectColor: String = "default-color";
   size: String = "default-size";
-  sizes: Array<Stock> = []
+  sizes: Array<String> = []
+  colors: Array<String> = []
   constructor(private service:ApiService, private activatedRoute:ActivatedRoute) { 
     this.id = this.activatedRoute.snapshot!.paramMap!.get("id")!;
     this.slug = this.activatedRoute.snapshot!.paramMap!.get("slug")!;
     this.service.getProductById(this.slug,this.id).subscribe(prd=> {
       this.product = prd.data
+      this.productColor = prd.data.color;
+      this.productSize = prd.data.size;
+      this.productPrice = prd.data.price
+      if(Array.isArray(prd.data.stocks)){
+        prd.data.stocks.map(stock=> {
+          if(!this.colors.includes(stock.color)){
+            this.colors.push(stock.color)
+          }
+        })
+      }
     })
   }
   selectedColor(){
@@ -29,7 +43,22 @@ export class ProductPageComponent implements OnInit {
     this.service.getStockFromProductByColor(this.id, this.selectColor).subscribe(res=> {
       if(Array.isArray(res.data)){
         res.data.map(stock => {
-          this.sizes.push(stock)
+          this.sizes.push(stock.size)
+        })
+      }
+    })
+    
+    this.productColor = this.selectColor;
+    this.productSize = "";
+  }
+  selectedSize(){
+    this.productSize = this.size;
+    this.service.getStockFromProductByColor(this.id, this.selectColor).subscribe(res=> {
+      if(Array.isArray(res.data)){
+        res.data.map(stock => {
+          if(stock.size == this.productSize){
+            this.productPrice = stock.price
+          }
         })
       }
     })
